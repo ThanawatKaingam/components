6153fdfa118753e7db62d36e2fcff8c43431b096
ThanawatKaingam <68525371+ThanawatKaingam@users.noreply.github.com>
2020-08-21 20:27:56 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="เลือกที่ชอบได้เลยครับ" />
+    <hr />
+    <div class="row">
+      <div class="col-6">
+        <img alt="Vue logo" height="400" width="500" src="./assets/1.jpg" />
+        <h1>{{total1}}</h1>
+      </div>
+      <div class="col-6">
+        <img alt="Vue logo" height="400" width="500" src="./assets/2.jpg" />
+        <h1>{{total2}}</h1>
+      </div>
+      <div class="col-6">
+        <CSButton @cc="sum" />
+      </div>
+      <div class="col-6">
+        <CSButton @cc="sum1" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import CSButton from "./components/CsButton";
+import HelloWorld from "./components/HelloWorld";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    CSButton,
+  },
+
+  data: function () {
+    return {
+      total1: 0,
+      total2: 0,
+    };
+  },
+
+  methods: {
+    sum(counterValue) {
+      this.total1 = counterValue;
+    },
+    sum1(counterValue) {
+      this.total2 = counterValue;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
